be358eea044882b574551a88a0d2626af2cb805f
samrat013 <jhaasuraj1234@gmail.com>
2025-10-11 21:44:50 +0530

    Queue using CF

--- queue/queue.java
+++ queue/queue.java
@@ -1,22 +1,45 @@
+import java.util.*;;
+
 public class queue {
     public static void main(String[] args)
     {
 
+        //Queue using collection framework
+        //1.LinkedList class
+        // Queue<Integer> q = new LinkedList<>();
+        // q.add(1);
+        // q.add(2);   
+        // q.add(3);
+        // while(!q.isEmpty()){
+        //     System.out.println(q.peek() + " is removed from Queue");
+        //     q.remove();
+        // }
+
+        //2.ArrayDeque class
+        // Queue<Integer> q1 = new ArrayDeque<>();
+        // q1.add(1);  
+        // q1.add(2);
+        // q1.add(3);
+        // while(!q1.isEmpty()){
+        //     System.out.println(q1.peek() + " is removed from Queue");
+        //     q1.remove();
+        // }
+
         //Queue using linked list
-        queue3 q = new queue3();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        q.add(4);
-        q.add(5);
-        q.add(6);
-        while(!q.isEmpty()){
-            System.out.println(q.peek() + " is removed from Queue");
-            q.remove();
-        }
-        System.out.println(q.peek());
-        q.add(7);
-        System.out.println(q.peek());
+        // queue3 q = new queue3();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // q.add(5);
+        // q.add(6);
+        // while(!q.isEmpty()){
+        //     System.out.println(q.peek() + " is removed from Queue");
+        //     q.remove();
+        // }
+        // System.out.println(q.peek());
+        // q.add(7);
+        // System.out.println(q.peek());
 
         // queue2 q = new queue2(5);
         // q.add(3);
